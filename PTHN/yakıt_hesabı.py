fa2819dd4513a6642761b184625c0487649434ba
1alpyigit <108079059+1alpyigit@users.noreply.github.com>
2023-11-17 15:25:37 +0300

    Python
    Giriş

---
+++ PTHN/yakıt_hesabı.py
@@ -0,0 +1,111 @@
+#sayı = 5
+#print(type(sayı))
+
+#x = int(input("Bir sayı giriniz = "))
+#if x >10:
+#    print("Bu sayı 10'dan büyüktür!")
+#elif x == 10:
+#    print("Bu sayı 10'a eşittir!")
+#elif x < 10:
+#    print("Bu sayı 10'dan küçüktür!")
+
+
+#km = int(input("kaç km gittin:"))
+#yakıt = float(input("km'de kaç kuruş yaktın:"))
+#print("Toplam tutar: ", km*yakıt)
+
+
+
+#num1 = float(input("Birinci sayıyı giriniz: "))
+#num2 = float(input("İkinci sayıyı giriniz: "))
+
+#islemseçin = input("Bir işlem seçin(+,-,*,/)?: ")
+#if islemseçin == "+":
+ #   print(num1, "+" , num2, "=", num1 + num2)
+#elif islemseçin == "-":
+ #   print(num1, "-", num2, "=" , num1 - num2)
+#elif islemseçin == "*":
+ #   print(num1, "*", num2, "=", num1 * num2)
+#elif islemseçin == "/":
+ #   print(num1, "/", num2, "=",  num1 / num2)
+#else:
+ #   print("Bu işlem geçerli değildir!")
+
+
+
+#x = 0
+#for x in range(10):
+#    print(x)
+
+
+
+#x = 0
+#while x < 5:
+#    print(x)
+#    x+=2
+
+
+
+#x = 0
+#while x < 20:
+#    print(x)
+#    if x == 18:
+#        break
+#    x += 1
+
+
+
+#x = 0
+#y = 0
+#for x in range(15):
+#    if x % 2 == 0:
+#        continue
+#    print(x)
+
+#for y in range(15):
+#    if y % 2 == 1:
+#        continue
+#    print(y)   
+
+
+
+#x = "Sen beni seviyor musun?"
+#y = "Sen beni sevmiyor mudundur?"
+
+#for i in x:
+#    if i not in y:
+#        print(i)
+
+
+
+#ilk_metin = "Merhaba, dünya!"
+#ikinci_metin = "merhaba, Dünya!"
+
+#if ilk_metin.lower() == ikinci_metin.lower():
+#    print("İki metin birbirine eşit.")
+#else:
+#    print("İki metin birbirinden farklı.")
+
+
+
+#x = "Yİğitalp'i çok seviyorum"
+#y = str(input("Aradığınız harf: "))
+
+#i=0
+
+#for sayı in x:
+#    if sayı == y:
+#        i +=1        
+#print(f"'{y}' sayı x'in içinde {i} kez geçiyor.")
+
+
+
+#x = str(input("Yazın!: "))
+#sayı = 0
+#for karakter in x:
+#    if karakter != '' and not karakter.isalnum():
+#        continue
+#    sayı += 1
+
+#print(f" Meyin içindeki sayı: {sayı}")    
+
