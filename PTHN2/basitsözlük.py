61c33479970343ed6ce7228269f5e17e7bd8fc77
1alpyigit <108079059+1alpyigit@users.noreply.github.com>
2023-11-20 00:51:11 +0300

    from_import
    basit sozluk yapma

---
+++ PTHN2/basitsözlük.py
@@ -0,0 +1,44 @@
+# sozluk_modulu.py
+#import os
+#sozluk = {
+#    "elma": "apple",
+#    "armut" : "pear",
+#    "çilek" : "strawberry",
+#    "portakal" : "orange"
+#}
+#def kelimeekle(kelime, anlamı):
+#    sozluk[kelime] = anlamı
+#    print(f"{kelime} kelimesi sözlüğe eklendi. ")
+#def ceviri(kelime):
+#    return sozluk.get(kelime, "Bu kelime sözlükte bulunamadı.")
+#kullan_soz.py
+#from sozluk_modulu.py import kelimeekle, cevir
+#while True:
+ #   os.system('cls')    
+#    print("""
+ #   ekle -1
+#    çevir -2
+ #   listele -3      
+ #   çıkış -4            
+ #   """)
+ #   secim = input("Seçim yapın: ")
+ #   if secim == "1":
+#        kelime = input("Eklemek istediğiniz kelime: ")
+ #       anlamı = input("Kelimenin anlamı: ")
+#        kelimeekle(kelime, anlamı)
+ #   elif secim == "2":
+#        kelime = input("Anlamını öğrenmek istediğin kelime: ")
+#        anlamı = cevir(kelime)
+#        print(f"{kelime} kelimenin anlamı: {anlamı}")
+#    elif secim == "3" :
+#        print("Kelime       Anlamı")
+#        for x in sozluk:
+#            print(x, sozluk.get(x))
+ #       print(len(sozluk), "sozluk listelendi")    
+ #   elif secim == "4":
+ #       print("Program çıkılıyor...")
+#        break
+#    else:
+#        print("Gereksiz seçim. Lütfen tekrar deneyin.")
+
+    
